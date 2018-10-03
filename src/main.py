7d0977d4a17450a29f06fef34148d2f2a4a5d345
Yábir García <yabirgb@gmail.com>
2018-10-04 01:55:28 +0200

    New urls

--- src/main.py
+++ src/main.py
@@ -7,7 +7,10 @@ import peewee_async
 
 from models.base import db
 
-from api.user import (UserHandler, ProfileManager)
+from api.v1.user import (UserHandler, ProfileManager)
+from api.v1.status import (StatusHandler, UserStatuses, FavouriteStatus,
+                            UnFavouriteStatus    
+                        )
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -17,7 +20,13 @@ def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
         (r'/api/v1/accounts/(?P<id>[\d+])', UserHandler),
-        (r'/api/v1/accounts/update_credentials', ProfileManager)
+        (r'/api/v1/accounts/(?P<id>[\d+])/statuses', UserStatuses),
+        (r'/api/v1/accounts/update_credentials', ProfileManager),
+
+        (r'/api/v1/statuses/', UserStatuses),
+        (r'/api/v1/statuses/(?P<pid>[\d+]+)', StatusHandler),
+        (r'/api/v1/statuses/(?P<pid>[\d+]+)/favourite', FavouriteStatus),
+        (r'/api/v1/statuses/(?P<pid>[\d+]+)/unfavourite', UnFavouriteStatus),
     ], debug=True)
 
 if __name__ == "__main__":

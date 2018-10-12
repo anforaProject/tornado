fe139fc1bf5ded80e5d7398295fbb587cf3e15a1
Yábir García <yabirgb@gmail.com>
2018-10-12 13:15:53 +0200

    Update base handler

--- src/api/v1/base_handler.py
+++ src/api/v1/base_handler.py
@@ -4,7 +4,7 @@ class BaseHandler(tornado.web.RequestHandler):
 
     def set_default_headers(self):
         self.set_header("Access-Control-Allow-Origin", "*")
-        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header("Access-Control-Allow-Headers", "*")
         self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
         self.set_header('Content-Type', 'application/json')
 
